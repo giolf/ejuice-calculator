ca0b7b6d559c4419abc3b6e9debc7535eb8f2caf
Giovanni Far <giovanni.far@gmail.com>
2015-09-15 01:36:55 +0000

    implementation layout flavoring table

--- controllers/ejcalc.js
+++ controllers/ejcalc.js
@@ -9,5 +9,24 @@ angular.module('app-ejcalc')
 			water 		: 10,
 			nicotine 	: 18
 		};
-
+		
+		$scope.targetP = {
+			amount 		: 10,
+			pg			: 10,
+			vg			: 10,
+			water		: 10,
+			nicotine 	: 10
+		};
+		
+		$scope.flavoringP = [{
+			name		: 'Lemon',
+			percentage	: 10,
+			composition	: 'PG'
+		},
+		{
+			name		: 'Peach',
+			percentage	: 15,
+			composition : 'VG'
+		}];
+		
 	}]);
