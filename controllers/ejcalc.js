81ce92d865c573d37d6ad5eecc8b884dd5b3d3de
Giovanni Far <giovanni.far@gmail.com>
2015-09-16 19:49:08 +0000

    Implementation flavoring panel's business logic

--- controllers/ejcalc.js
+++ controllers/ejcalc.js
@@ -1,16 +1,16 @@
 'use strict';
 
 angular.module('app-ejcalc')
-	.controller('ejcalc', ['$scope', function ($scope) {
+	.controller('ejcalc', ['$scope', function (scope) {
 
-		$scope.nblP = {
+		scope.nblP = {
 			pg 			: 60,
 			vg 			: 40,
 			water 		: 10,
 			nicotine 	: 18
 		};
 		
-		$scope.targetP = {
+		scope.targetP = {
 			amount 		: 10,
 			pg			: 10,
 			vg			: 10,
@@ -18,15 +18,24 @@ angular.module('app-ejcalc')
 			nicotine 	: 10
 		};
 		
-		$scope.flavoringP = [{
-			name		: 'Lemon',
-			percentage	: 10,
-			composition	: 'PG'
-		},
-		{
-			name		: 'Peach',
-			percentage	: 15,
-			composition : 'VG'
-		}];
+		scope.flavoringP = {
+			newFlavor : {
+				name 		: 'Mint',
+				percentage 	: 10,
+				composition : 'PG'
+			},
+			flavorList : []
+		};
 		
+		scope.add = function() {
+			scope.flavoringP.flavorList.push({
+				name		: scope.flavoringP.newFlavor.name,
+				percentage	: scope.flavoringP.newFlavor.percentage,
+				composition	: scope.flavoringP.newFlavor.composition
+			});
+		};
+		
+		scope.remove = function(index) {
+			scope.flavoringP.flavorList.splice(index, 1);
+		}
 	}]);
